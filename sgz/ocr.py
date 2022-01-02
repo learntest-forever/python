f66adb23de06b0c71279fb0798602b3b0cdc39f3
unknown <dongzhiqiang@megvii-inc.com>
2022-01-02 15:40:58 +0800

    sgz

---
+++ sgz/ocr.py
@@ -0,0 +1,18 @@
+#-*- coding: UTF-8 -*-
+from PIL import Image
+# import tesseract
+import pytesseract
+
+
+if __name__ == "__main__":
+    # reader = easyocr.Reader(['ch_sim','en'])
+    # result = reader.readtext("D:\project\FR\script\pic1.png")
+    # print(result)
+
+    text = pytesseract.image_to_string(Image.open(r'D:\code\mypython\python\pic2.png'),lang='chi_sim')
+    print(text)
+    print("end")
+    text = pytesseract.image_to_string(Image.open(r'D:\code\mypython\python\pic2.png'))
+    print(text)
+
+
